20d1b09eeb68bd10a734f6df21b3959c947c064d
anjero <anjero@anjero.cn>
2018-02-07 11:30:27 +0800

    update mumtech.

--- mumtech/activity/healthysleep/js/healthysleep.js
+++ mumtech/activity/healthysleep/js/healthysleep.js
@@ -5,9 +5,25 @@ Zepto(function ($) {
     // $(".container").height(height);
 
 
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    var isHWOK = width < height;
+    if (!isHWOK) {
+        console.log("not android or ios");
+        $(".container,.footer,.alert-m").width(375 / 667 * height);
+    } else {
+        console.log("is Android:" + isAndroid + ";is Ios:" + isiOS)
+    }
+
     $(".close").click(function () {
         $(this).parent().hide();
     });
+
+    $(".ex-button").click(function () {
+        $("#ex-alert").show();
+    });
+
 });
 
 
